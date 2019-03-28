06d85471d02647194c7cb0e0a55a11b3e84d6015
claimapp <claimapp@xlifang.com>
2019-03-28 09:51:02 +0800

    Xfermode

--- app/src/main/java/com/xibei/kotlinanimation/MainActivity.kt
+++ app/src/main/java/com/xibei/kotlinanimation/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.xibei.kotlinanimation
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.xibei.kotlinanimation.activity.DemoActvity1
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    override fun onClick(v: View?) {
+        when(v?.id){
+            R.id.btn1 -> startActivity(Intent(this@MainActivity,DemoActvity1::class.java))
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn1.setOnClickListener(this@MainActivity)
     }
 }
---
+++ app/src/main/java/com/xibei/kotlinanimation/activity/DemoActvity1.kt
@@ -0,0 +1,14 @@
+package com.xibei.kotlinanimation.activity
+
+import android.app.Activity
+import android.os.Bundle
+import com.xibei.kotlinanimation.R
+
+class DemoActvity1: Activity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_activity1)
+    }
+
+}
